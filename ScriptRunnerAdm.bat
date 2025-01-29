00c196ab308bcacc9a13c1a361c76f3b7fa15b7f
marscripts <mahdi.rahimi@ifp.ag>
2025-01-29 11:11:53 +0100

    updates

--- ScriptRunnerAdm.bat
+++ ScriptRunnerAdm.bat
@@ -1,25 +1,7 @@
 @echo off
-:: Check for administrative privileges
-net session >nul 2>&1
-if %errorlevel% neq 0 (
-    :: Relaunch the batch file with administrator privileges
-    echo Requesting administrative privileges...
-    powershell.exe -Command "Start-Process '%~f0' -Verb RunAs"
-    exit /b
-)
+:: Get the directory where the batch file is located
+set "scriptPath=%~dp0netzwerkprotokolle.ps1"
 
-:: Path to the PowerShell script
-set PowerShellScript=".\netzwerkprotokolle.ps1"
+:: Check if PowerShell is running as administrator
+powershell -Command "Start-Process PowerShell -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File \"%scriptPath%\"' -Verb RunAs"
 
-:: Check if the PowerShell script exists
-if not exist %PowerShellScript% (
-    echo PowerShell script not found at: %PowerShellScript%
-    echo Please verify the path and try again.
-    pause
-    exit /b
-)
-
-:: Run the PowerShell script as administrator
-echo Running PowerShell script as administrator...
-powershell.exe -NoProfile -ExecutionPolicy Bypass -File %PowerShellScript%
-pause
